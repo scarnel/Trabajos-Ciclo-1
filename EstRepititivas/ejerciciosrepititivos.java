f7728099f8ecfa0fc1963d80805d663d10ad2722
oscardani <oscarquispe011@gmail.com>
2022-05-12 13:35:30 +0000

    repitivas

--- EstRepititivas/ejerciciosrepititivos.java
+++ EstRepititivas/ejerciciosrepititivos.java
@@ -1,6 +1,10 @@
 package EstRepititivas;
 
+import java.util.Scanner;
+
 public class ejerciciosrepititivos {
+
+    static Scanner lt=new Scanner(System.in);
 
 public static void ejer41While() {
     int contador=1;
@@ -14,7 +18,20 @@ public static void ejer41While() {
     System.out.println("la respuesta es:"+suma);
 }
 
-    public static void main(String[] args) {
-        ejer41While();
+    public static void pregunta2MP() {
+        System.out.println("ingrese el operador:");
+        char operador=lt.next().charAt(0);
+        switch (operador) {
+            case '+' :System.out.println("debes sumar "); break;
+            case '%' :System.out.println("debes sacr modulo"); break;
+        
+            default:
+                break;
+        }
     }
+
+    public static void main(String[] args) {
+        pregunta2MP();
+    }
+
 }
---
+++ EstRepititivas/exam3.java
@@ -0,0 +1,16 @@
+package EstRepititivas;
+
+import java.util.Scanner;
+
+/**
+ * exam3
+ */
+public class exam3 {
+
+Scanner lt=new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //definir varible
+        
+    }
+}
