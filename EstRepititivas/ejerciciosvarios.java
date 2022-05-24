4a24eeee68ff10b11368c62995cb0826ca9faaa0
oscardani <oscarquispe011@gmail.com>
2022-05-24 18:04:43 +0000

    numeros

--- EstRepititivas/ejerciciosvarios.java
+++ EstRepititivas/ejerciciosvarios.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 
 public class ejerciciosvarios {
     
-    static Scanner lT=new Scanner(System.in);
+    static Scanner leerT=new Scanner(System.in);
 
     public static void factorialXnum() {
         int n; 
         long result=1;
         System.out.println("Ingrese un numero:");
-        n=lT.nextInt();
+        n=leerT.nextInt();
         if(n>1){
         for (int i = 1; i <=n; i++) {
             result=result*i; 
@@ -20,38 +20,45 @@ public class ejerciciosvarios {
         System.out.println("El factorial de "+n+" es:"+result);
     }
 
-    public static long factorialnum(int n) {
+    public static long factorialNum(int n) {       
         long result=1;
         if(n>1){
         for (int i = 1; i <=n; i++) {
             result=result*i; 
             }
-        }
-        //System.out.println("El factorial de "+n+" es:"+result);
+        }       
         return result;
-    }
+    }    
 
-    public static BigInteger factorialnumbig(int n) {
+    public static BigInteger factorialNumBig(int n) {        
         BigInteger result=new BigInteger("1");
         if(n>1){
         for (int i = 1; i <=n; i++) {
             result=result.multiply(new BigInteger(String.valueOf(i))); 
             }
-        }
-        //System.out.println("El factorial de "+n+" es:"+result);
+        }       
         return result;
+    }    
+
+    public static void funcionExponencial(int x) {
+        double result=0;
+        for (int i = 0; i < x; i++) {
+            result=result+(Math.pow(x, i)/factorialNum(i));
+        }
+        System.out.println("Funcion Exponencia es: "+result);
     }
 
     public static void main(String[] args) {
         /*factorialXnum();
-        long resultado=factorialnum(10);
-        System.out.println("El resultado es :"+resultado);*/
-
-        System.out.println("ingrese un numero: ");
-        int numero=lT.nextInt();
+        long resultado=factorialNum(5);
+        System.out.println("Resultado:"+resultado);*/        
+        /*System.out.println("Ingrese un numero:");
+        int numero=leerT.nextInt();
         for (int i = 1; i <= numero; i++) {
-            System.out.println("Factorial de "+i+"¡ = " + factorialnumbig(i));
-        }
+            System.out.println("Factorial de "+i+"! =" + factorialNumBig(i));
+        }*/
+
+        funcionExponencial(5);
 
     }
 
